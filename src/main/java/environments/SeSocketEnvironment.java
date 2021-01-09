2dfb2dba64539491f38a3e157b1edfc6e84312c7
Premek Paska <premysl.paska@goodai.com>
2021-01-09 13:07:44 +0100

    SeSocketReaderWriter -- dead end (for now).

--- src/main/java/environments/SeSocketEnvironment.java
+++ src/main/java/environments/SeSocketEnvironment.java
@@ -3,33 +3,34 @@ package environments;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import communication.adapters.EntityAdapter;
-import communication.adapters.EntityTypeAdapter;
-import spaceEngineers.SeRequest;
-import world.LegacyEntity;
-import world.LegacyEntityType;
 
+import eu.iv4xr.framework.spatial.Vec3;
+import nl.uu.cs.aplib.mainConcepts.Environment;
+import nl.uu.cs.aplib.utils.Pair;
+import spaceEngineers.SeRequest;
+import world.Observation;
+
+import java.io.IOException;
 import java.lang.reflect.Modifier;
 
-public class SeSocketEnvironment extends SocketEnvironment {
+public class SeSocketEnvironment extends Environment {
 
     public static final int DEFAULT_PORT = 9678;
 
     public SeSocketEnvironment(String host, int port) {
-        super(host, port);
+        socket = new SeSocketReaderWriter(host, port);
     }
 
     public Object sendCommand(EnvOperation cmd) {
         return sendCommand_(cmd);
     }
 
+    private final SeSocketReaderWriter socket;
+
     // transient modifiers should be excluded, otherwise they will be send with json
-    // TODO(PP): replace entity adapters
     private static Gson gson = new GsonBuilder()
             .serializeNulls()
             .excludeFieldsWithModifiers(Modifier.TRANSIENT)
-            .registerTypeAdapter(LegacyEntityType.class, new EntityTypeAdapter())
-            .registerTypeHierarchyAdapter(LegacyEntity.class, new EntityAdapter())
             .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
             .create();
 
@@ -45,9 +46,36 @@ public class SeSocketEnvironment extends SocketEnvironment {
         // WP note:
         // the actual id of the agent and the id of its target (if it interacts with
         // something) are put inside the req object ... :|
-        String json = (String) sendCommand("APlib", "Se", "request", gson.toJson(req));
+        return (T) sendCommand("APlib", "Se", "request", gson.toJson(req));
         // we do not have to cast to T, since req.responseType is of type Class<T>
         //System.out.println(json);
         return gson.fromJson(json, req.responseType);
+    }
+
+    /**
+     * @param cmd representing the command to send to the real environment.
+     * @return an object that the real environment sends back as the result of the
+     * command, if any.
+     */
+    @Override
+    protected Object sendCommand_(EnvOperation cmd) {
+
+        try {
+            Observation obs = sendPackage(cmd.arg) ;
+            return Observation.toWorldModel(obs) ;
+        }
+        catch (IOException ex) {
+            System.out.println("I/O error: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * Primitive for sending a command-package to the game, and to return its response.
+     * The command to send should be wrapped as a "Request" object.
+     */
+    private <T> T sendPackage(SeRequest<T> packageToSend) throws IOException {
+        socket.write(packageToSend);
+        return socket.read(packageToSend.responseType);
     }
 }
